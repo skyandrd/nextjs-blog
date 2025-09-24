85a3a6f3aa9b0db18fdcef6235512e2f8c703c93
a <skyandrd@gmail.com>
2025-09-24 10:45:00 +0500

    fix: updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,10 +29,6 @@ export default function Home({
             skyandrd
           </a>
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
